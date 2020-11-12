5aad3a216a1967a158e7eaad4c16758d96a3fd7c
ocfbnj <ocfbnj@qq.com>
2020-11-12 21:03:32 +0800

    add binary gcd algorithm.

--- Algorithms/NumberTheoretic/gcd.cpp
+++ Algorithms/NumberTheoretic/gcd.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 int gcd(int a, int b) {
@@ -8,6 +9,32 @@ int gcd(int a, int b) {
     return gcd(b, a % b);
 }
 
+int bgcd(int a, int b) {
+    // make sure a >= b.
+    if (a < b) {
+        std::swap(a, b);
+    }
+
+    if (b == 0) {
+        return a;
+    }
+
+    bool a_isodd = a & 1;
+    bool b_isodd = b & 1;
+
+    if (a_isodd && b_isodd) {
+        return bgcd((a - b) >> 1, b);
+    } else if (a_isodd && !b_isodd) {
+        return bgcd(a, b >> 1);
+    } else if (!a_isodd && b_isodd) {
+        return bgcd(a >> 1, b);
+    }
+
+    // both a and b are even numbers.
+    return bgcd(a >> 1, b >> 1) << 1;
+}
+
 int main() {
-    std::cout << gcd(15, 39) << "\n";
+    std::cout << gcd(25, 125) << "\n";
+    std::cout << bgcd(25, 125) << "\n";
 }
